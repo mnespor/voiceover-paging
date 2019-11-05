ddfda330cfb2627794992fab1375eb35ff3d72e5
mnespor <mnespor@gmail.com>
2019-11-04 19:29:28 -0600

    initial commit

--- voiceover-paging/ViewController.swift
+++ voiceover-paging/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+  let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -18,3 +19,24 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return colors.count
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "blah", for: indexPath)
+    cell.contentView.backgroundColor = colors[indexPath.item]
+    return cell
+  }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return collectionView.frame.size
+  }
+}
